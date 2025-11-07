9dcdfdd5e6ab9e26ecdb39a0c3e28130a04b50ee
ghustdev <gustavocardoso0019@gmail.com>
2025-11-06 23:42:09 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-//          Componente React inicial
+//  Componente React inicial
 function Home() {
     return <h1>Servidor no ar! e conhecimento sobre Git adiquirido com sucesso!</h1>
 }
 
-//          Exportar função Home para renderizar página
+//  Exportar função Home para renderizar página
 export default Home;
