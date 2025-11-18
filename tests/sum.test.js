7b69634921d9f224a07b87d69a73787e26f875e4
ghustdev <gustavocardoso0019@gmail.com>
2025-11-18 17:23:52 -0300

    feat: finished day 15

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,2 +1,11 @@
-const sum = require('./sum');
+const sum = require('../models/sum.js');
+
+test('add 1 + 2 to equal 3', () => {
+  const res = sum(1, 2);
+  expect(res).toBe(3);
+});
+
+test('teste', () => {
+  console.log('testando');
+});
 
